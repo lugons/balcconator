b45f0eab068c11c098629ab02e2b1dfff53de45c
Andrija Prčić <bandigarf@lugons.org>
2012-08-16 23:49:26 +0200

    fetch_permissions decorator function

--- balcconator.py
+++ balcconator.py
@@ -180,6 +180,23 @@ def check_permissions(f):
     return decorated_function
 
 
+def fetch_permissions(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        g.username = session.get('username', None)
+        if not username:
+            g.permission_news = False
+            g.permission_reviewer = False
+
+        else:
+            user = Person.query.filter_by(username=username).first()
+            g.permission_news = user.permission_news
+            g.permission_reviewer = user.permission_reviewer
+
+        return f(*args, **kwargs)
+    return decorated_function
+
+
 def permission_news(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
